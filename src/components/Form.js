bc3c9a1a4a60e3d7ef6878fe6300714bcd5876e0
Grace <110872110+GraceyoobeeGit@users.noreply.github.com>
2022-10-19 13:25:06 +1300

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,36 @@
 import "./FormStyles.css";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-import React from 'react'
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'service_jzumcfa', 
+      'template_djgunur', 
+      form.current, 
+      'MGkOpYaTvH-CgCW6r')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return <div className="form">
-    <form>
-        <label>Your Name</label>
-        <input type="text"></input>
-        <label>Email</label>
-        <input type="email"></input>
-        <label>subject</label>
-        <input type="text"></input>
-        <label>Message</label>
-        <textarea rows="6" placeholder="Type your message here" />
-    <button className="btn">Submit</button>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" rows={8} />
+      <input className="btn" type="submit" value="Send" />
     </form>
   </div>;
 }
